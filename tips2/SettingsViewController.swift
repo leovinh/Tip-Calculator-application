c18f266f790e33ab01a383c051ec9780552c3780
The Vinh Duong <thevinhduong@gmail.com>
2016-02-28 17:33:44 +0700

    re-submit

---
+++ tips2/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  tips2
+//
+//  Created by The Vinh Duong on 2/23/16.
+//  Copyright © 2016 The Vinh Duong. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    
+    
+   
+    
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
