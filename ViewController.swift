1e82ddbfece79af6f070be6abd0c20b9684300f0
The Vinh Duong <thevinhduong@gmail.com>
2016-02-28 17:29:34 +0700

    Move file from tips2 to root
    don't know why can't push that file to github

---
+++ ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  tips2
+//
+//  Created by The Vinh Duong on 2/20/16.
+//  Copyright © 2016 The Vinh Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+  
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "\(tip)"
+        totalLabel.text = "\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+     
+        
+    }
+    
+    
+       
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
